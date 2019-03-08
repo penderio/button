4cdd9bd6fd08a9ff8aefd32e1d4d2e445880f9af
keyflight <olivier@andev.nl>
2019-03-08 11:52:08 +0100

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,79 @@
 import React, {Component} from 'react'
+import {injectGlobal} from 'emotion'
 import {render} from 'react-dom'
+import plus from '@cmds/icons/es/plus'
 
-import Example from '../../src'
+injectGlobal`
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+    }
+`
+
+import Button from '../../src'
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>button Demo</h1>
-      <Example/>
-    </div>
-  }
+    render() {
+        return <div>
+            <h1>Button Demo</h1>
+            <h2>
+                Default
+            </h2>
+            <Button>Click here</Button>
+            <h2>
+                With icon
+            </h2>
+            <Button
+                icon={plus}
+            >
+                Create
+            </Button>
+            <h2>
+                Hover / focus
+            </h2>
+            <Button
+                icon={plus}
+                hover
+            >
+                Create
+            </Button>
+            <h2>
+                Active
+            </h2>
+            <Button
+                icon={plus}
+                active
+            >
+                Create
+            </Button>
+            <h2>
+                With icon, without text
+            </h2>
+            <Button
+                icon={plus}
+            />
+            <h2>
+                Highlighted
+            </h2>
+            <Button
+                icon={plus}
+                highlighted
+            >
+                Create
+            </Button>
+            <h2>
+                Primary
+            </h2>
+            <Button
+                icon={plus}
+                primary
+            >
+                Create
+            </Button>
+        </div>
+    }
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,99 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {css, cx} from 'emotion'
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+const Button = ({onClick, type, className, icon, highlighted, primary, active, hover, focus, children}) => (
+    <button
+        type={type || 'button'}
+        className={cx(css`
+            background: #fff;
+            border: none;
+            border-radius: 5px;
+            box-shadow: 0 1px 2px 0 rgba(0,0,0,.25);
+            color: #262626;
+            cursor: pointer;
+            fill: currentColor;
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 30px;
+            padding: 0 10px;
+            height: 30px;
+            position: relative;
+            text-align: center;
+            text-decoration: none;
+            transition: all .05s ease-out;
+            user-select: none;
+            white-space: nowrap;
+            display: flex;
+            align-items: center;
+            &:focus, &:hover {
+                -webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.25);
+                -webkit-transform: translateY(-1px);
+                box-shadow: 0 2px 2px 0 rgba(0,0,0,.25);
+                transform: translateY(-1px);
+                outline: none;
+            }
+            &:active {
+                -webkit-box-shadow: 0 1px 2px 0 rgba(0,0,0,.25);
+                -webkit-transform: translateY(1px);
+                box-shadow: 0 1px 2px 0 rgba(0,0,0,.25);
+                transform: translateY(1px);
+            }
+        `,
+            hover || focus ? css`
+                -webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,.25);
+                -webkit-transform: translateY(-1px);
+                box-shadow: 0 2px 2px 0 rgba(0,0,0,.25);
+                transform: translateY(-1px);
+                outline: none;
+            ` : null,
+            active ? css`
+                -webkit-box-shadow: 0 1px 2px 0 rgba(0,0,0,.25);
+                -webkit-transform: translateY(1px);
+                box-shadow: 0 1px 2px 0 rgba(0,0,0,.25);
+                transform: translateY(1px);
+            ` : null,
+            primary ? css`
+            background-color: #07f;
+            color: #fff;
+            font-weight: 700;
+            ` : css`
+
+            `,
+            highlighted ? css`
+                &:before {
+                    background-color: rgba(0,119,255,.1);
+                    border: 3px solid #fff;
+                    border-radius: 5px;
+                    bottom: 0;
+                    content: "";
+                    left: 0;
+                    position: absolute;
+                    right: 0;
+                    top: 0;
+                }
+                &:after {
+                    border: 1px solid #07f;
+                    border-radius: 5px;
+                    bottom: 0;
+                    content: "";
+                    left: 0;
+                    position: absolute;
+                    right: 0;
+                    top: 0;
+                }
+            ` : null,
+            className
+        )}
+        onClick={onClick}
+    >
+        {icon ? icon({
+            height: 16,
+            className: children ? css`
+                margin-right: 4px;
+            ` : null
+        }) : null}
+        {children}
+    </button>
+)
+
+export default Button
